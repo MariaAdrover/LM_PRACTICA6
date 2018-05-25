5391864b2e2e6bb16070bfc7a0d3d8525ab87c23
MariaAdrover <32370320+MariaAdrover@users.noreply.github.com>
2018-05-25 16:54:26 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -9,7 +9,6 @@ var margin;
 
 var foto = 5; 
 
-/* $(function(){...}); --> window.onload = function() {...};*/
 $(document).ready(function() {	
 	
 	/*Activar tooltips*/
@@ -20,18 +19,17 @@ $(document).ready(function() {
 		return false;
 	});
 	
-	/*determinar a cuantos pixeles se fija la barra del menu*/
-	var navBar = $(".navbar-collapse");
-	offsetNav=navBar.offset();
+	/*determinar a cuantos pixeles se fija la barra del menu*/	
+	offsetNav=$(".navbar-collapse").offset();
 	$(".navbar-inverse").attr('data-offset-top', offsetNav.top);	
 	
-	/*El menu NO es fijo para dispositivos de menos de 768px*/
+	/*El menu no es fijo para dispositivos de menos de 768px*/
 	if ((window.matchMedia('(max-width: 767px)').matches)) {
 		$('nav').removeAttr('data-spy');
 		$('nav').removeAttr('data-offset-top');
 	}
 	
-	/*-----funcion boton volver------*/
+	/*-----Volver arriba al pulsar botón volver------*/
 	$('#botonVolver').click(function(){	
 		if ((window.matchMedia('(min-width: 768px)').matches)) {
 			$('#news1').focus();
@@ -40,33 +38,11 @@ $(document).ready(function() {
 		}		
 	});
 	
-	/*Evento al clicar sobre una imagen*/
-	/*Mostrar/ocultar modal*/	
+	/*Evento al clicar sobre una imagen: Mostrar/ocultar modal*/
 	$(document).on("click", "picture", function(evt){
 	   var urlImg = 'img/n' + ($(evt.target).attr("id")) + 'big.jpg';
 	   $("#bigImg img").attr("src",urlImg);
 	   $("#bigImg").modal("show");
-	   
-	   /*Tamaño visor de imágenes*/
-		if (window.matchMedia('(min-width:768px)').matches) {
-			ajustarVisor();
-		}
-	});
-	
-	/*Cargar noticias al pulsar boton */
-	$('#boton1').click(function() { /*revisar EL BOTON HA DE ESTAR OPERATIVO SOLO SI EL AUTOSCROLL ESTA DESACTIVADO???*/
-		if (data < 6) {
-			/*Mostrar ventana de carga*/
-			$('#loading').show();
-			$.getJSON( 'https://rawgit.com/MariaAdrover/LM_PRACTICA6/v0.4/data/data' + data + '.json', function(jsonObject) {
-				afegirBloc(jsonObject);
-				/*Ocultar ventana de carga*/
-				$('#loading').hide();
-			});
-			data++;
-		} else {
-			$('#boton1').text('NO HAY MÁS NOTICIAS');
-		}
 	});
 	
 	/* ------ ACTIVAR/DESACTIVAR AUTOSCROLL -----*/
@@ -80,7 +56,12 @@ $(document).ready(function() {
 		}
 	});	
 	
-	/*Cargar noticias al hacer scroll*/	
+	/*Cargar noticias al pulsar boton */
+	$('#boton1').click(function() {
+		getData(0);
+	});
+	
+	/*Eventos al hacer scroll*/	
 	$(window).scroll(function() {
 		
 		/*Ajustar padding del div de contenido para que no de tirones al hacer scroll y fijarse el menú arriba*/		
@@ -88,34 +69,18 @@ $(document).ready(function() {
 			ajustarPadding();
 		}
 		
+		/*Cargar noticias*/
 		if (auto) {
-			if ($(window).scrollTop() + $(window).height() >= $(document).height() - 100) { /*-------REVISAR offset*/
-				if (data < 6) {
-					/*Mostrar ventana de carga*/
-					$('#loading').show();
-					auto=false;
-					/*Cargar noticias*/
-					$.getJSON('https://rawgit.com/MariaAdrover/LM_PRACTICA6/v0.4/data/data' + data + '.json', function(jsonObject) {
-						afegirBloc(jsonObject);
-						/*Ocultar ventana de carga*/
-						$('#loading').hide();
-						auto=true;
-					});
-					data++;
-				} else {
-					$('#boton1').text('NO HAY MÁS NOTICIAS');
-				}			
+			if ($(window).scrollTop() + $(window).height() >= $(document).height() - 100) {
+				getData(1);			
 			}
 		}
 		
-		/*Mostrar/Ocultar botones AutoScroll y volver arriba*/
-		finPubli=200+offsetNav.top;
-		
-		if ($(window).scrollTop()>finPubli) {
+		/*Mostrar/Ocultar botones AutoScroll y volver arriba*/		
+		if ($(window).scrollTop()>=(offsetNav.top+200)) {
 			$('#autoscroll').show();
 			$('#botonVolver').show();			
-		}
-		if ($(window).scrollTop()<finPubli) {			
+		} else {			
 			$('#autoscroll').hide();
 			$('#botonVolver').hide();	
 		}
@@ -157,8 +122,25 @@ function ajustarPadding() {
 		$('#contenido').css('margin-top', 0);
 	}
 }
-	
-function ajustarVisor() {
-	var ancho = $("#bigImg img").width() + 'px';
-	$(".modal-dialog").css("width",900);	
+
+function getData(evento) {
+	if (data < 6) {
+		/*Mostrar ventana de carga*/
+		$('#loading').show();
+		if (evento==1) {
+			auto=false;
+		}
+		/*Cargar noticias*/
+		$.getJSON('https://rawgit.com/MariaAdrover/LM_PRACTICA6/v0.4/data/data' + data + '.json', function(jsonObject) {
+			afegirBloc(jsonObject);
+			/*Ocultar ventana de carga*/
+			$('#loading').hide();
+			if (evento==1) {
+				auto=true;
+			}
+		});
+		data++;
+	} else {
+		$('#boton1').text('NO HAY MÁS NOTICIAS');
+	}	
 }
